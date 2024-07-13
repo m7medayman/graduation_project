103374d61b4f629c56fd57a78671c69f1e845a96
m7medayman <m7medmahgoop@gmail.com>
2024-07-13 08:27:36 +0300

    1.3

--- ui/sensory_model.py
+++ ui/sensory_model.py
@@ -4,10 +4,10 @@ from gpio_module.vlx1_tof_sensor.vlx_module import VlxModule
 class SensoryModel:
     def __init__(self)->None:
         self.vlxModule=VlxModule()
-        self._sensorX=KFilter(value=self.vlxModule.getSensorX())
-        self._sensorY=KFilter(value=self.vlxModule.getSensorY())
-        self.sensorXVlaue=0
-        self.sensorYVlaue=0
+        self._sensorXFilter=KFilter(value=self.vlxModule.getSensorX())
+        self._sensorYFilter=KFilter(value=self.vlxModule.getSensorY())
+        self.sensorXValue=0
+        self.sensorYValue=0
     
     def setCenter(self):
         meanSensorX=0
@@ -17,8 +17,8 @@ class SensoryModel:
             valueY = self.vlxModule.getSensorY()
             meanSensorX+=valueX
             meanSensorY+=valueY
-            self.sensorXValue=self.kFilter.stimate(valueX)
-            self.sensorYValue=self.kFilter.stimate(valueY)
+            self.sensorXValue=self._sensorXFilter.stimate(valueX)
+            self.sensorYValue=self._sensorYFilter.stimate(valueY)
             ti.sleep(0.001)
         meanSensorX=meanSensorX/50
         meanSensorY=meanSensorY/50
@@ -26,16 +26,16 @@ class SensoryModel:
         self.vlxModule.setCenterY(meanSensorY)
 
     def getSensorX(self):
-        sensorX=self._sensorX.stimate(self.vlxModule.getSensorX())
+        sensorX=self._sensorXFilter.stimate(self.vlxModule.getSensorX())
         return sensorX
     
     def getSensorY(self):
-        sensorY=self._sensorX.stimate(self.vlxModule.getSensorY())
+        sensorY=self._sensorXFilter.stimate(self.vlxModule.getSensorY())
         return sensorY
 
     def _updateSensors(self):
-        self.sensorXValue=self._sensorX.stimate(self.vlxModule.getSensorX())
-        self.sensorYValue=self._sensorY.stimate(self.vlxModule.getSensorY())
+        self.sensorXValue=self._sensorXFilter.stimate(self.vlxModule.getSensorX())
+        self.sensorYValue=self._sensorYFilter.stimate(self.vlxModule.getSensorY())
 
     def updateLoop(self):
         self._updateSensors()
